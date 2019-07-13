cb6c18ee77ffbcf72d9ea88182a5c4181d8a58fd
mohithmp <41140068+mohithmp@users.noreply.github.com>
2019-07-13 10:38:26 -0700

    Initial Commit

---
+++ MLtrail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLtrail
+//
+//  Created by Mohith Mullaguru Prabhakar on 7/13/19.
+//  Copyright © 2019 Mohith Mullaguru Prabhakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

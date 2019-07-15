8731d9b95e266333769affa5649f985f95facd1d
Mohith Mullaguru Prabhakar <mohithmullaguruprabhakar@Mohiths-MacBook-Pro.local>
2019-07-15 11:41:11 -0700

    First commit

--- MLtrail/ViewController.swift
+++ MLtrail/ViewController.swift
@@ -7,12 +7,67 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // here is where we start camera
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video)
+            else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice)
+            else {return}
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self , queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        //let request = VNCoreMLRequest(model: VNCoreMLModel,
+          //                            completionHandler: VNRequestCompletionHandler?)
+        //VNImageRequestHandler(CGImage: CGImage, options: [:].perform(request: [VNREquest]))
+        
+        }
+        func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection){
+       // print("Camera was able to capture a frame:", Date())
+            
+            guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+                else {return}
+            
+            // Here is where trainied ML Model goes
+            guard let model = try? VNCoreMLModel(for: Resnet50().model)
+                else { return }
+            let request = VNCoreMLRequest(model: model)
+            {
+                (finishedReq, err) in
+                
+                // Perhaps check err
+                
+                //print(finishedReq.results)
+                
+                guard let results = finishedReq.results as?
+                    [VNClassificationObservation] else {return}
+                
+                guard let firstObservation = results.first else
+                { return }
+                
+               
+                print(firstObservation.identifier, firstObservation.confidence)
+                
+            }
+            try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
     }
 
 
